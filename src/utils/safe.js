f092cd66d8444af560c4aad991217486ab0aa7f0
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2022-05-24 07:15:37 +0530

    update

--- src/utils/safe.js
+++ src/utils/safe.js
@@ -5,35 +5,53 @@ import Web3 from 'web3'
 import CreateDAO  from '../abis/DAO.json'
 import store from '../redux/store'
 
-export async function initiateConnection(owners, threshold, dispatch, tokenName, tokenSymbol, totalDeposit, minDeposit, maxDeposit, ownerFee, closeDate, feeUSDC, quoram, formThreshold) {
+async function gnosisSafePromise(owners, threshold, dispatch) {
+  try{
+
     const safeFactory = await SafeFactory.create({ ethAdapter })
 
     const safeAccountConfig = {
-        owners,
-        threshold,
-      }
+      owners,
+      threshold,
+    }
     const safeSdk = await safeFactory.deploySafe({ safeAccountConfig })
-    const newSafeAddress = safeSdk.getAddress()
 
+    const newSafeAddress = safeSdk.getAddress()
     dispatch(safeConnected(newSafeAddress, safeSdk))
-    console.log("newSafeAddress: ", newSafeAddress)
-    const days = Math.round((new Date(closeDate) - new Date()) / (1000 * 60 * 60 * 24))
-    const web3 = new Web3('https://ropsten.infura.io/v3/feaf3bb22fef436e996b4eb0e157dacd');
-    const abi = CreateDAO.abi
-    const address = '0xd4a461302d8c58A7EE9baC3734F17Be17f23D2dA'
-    const contract = new web3.eth.Contract(abi, address)
-    const value = contract.methods.createDAO(
-      tokenName, 
-      tokenSymbol, 
-      totalDeposit, 
-      minDeposit, 
-      maxDeposit, 
-      ownerFee, 
-      days, 
-      feeUSDC, 
-      newSafeAddress, 
-      quoram, 
-      formThreshold
-    ).call();
-    console.log("value: ", value)
+    return newSafeAddress
+  }
+  catch{
+    return "Gnosis safe connection cannot be established!"
+  }
+  
+}
+
+export async function initiateConnection(owners, threshold, dispatch, tokenName, tokenSymbol, totalDeposit, minDeposit, maxDeposit, ownerFee, closeDate, feeUSDC, quoram, formThreshold) {
+
+  await gnosisSafePromise(owners, threshold, dispatch)
+    .then((safeAddress) => {
+      console.log(successMsg)
+      const days = Math.round((new Date(closeDate) - new Date()) / (1000 * 60 * 60 * 24))
+      const web3 = new Web3('https://rinkeby.infura.io/v3/feaf3bb22fef436e996b4eb0e157dacd')
+      const abi = CreateDAO.abi
+      const address = '0xbfa33C88f614345F3cbaedB8174db04043E3E412'
+      const contract = new web3.eth.Contract(abi, address)
+      const value = contract.methods.createDAO(
+        tokenName, 
+        tokenSymbol, 
+        totalDeposit, 
+        minDeposit, 
+        maxDeposit, 
+        ownerFee, 
+        days, 
+        feeUSDC, 
+        safeAddress, 
+        quoram, 
+        formThreshold
+      ).call()
+      console.log(value)
+    })
+  .catch((errorMsg) => {
+    console.log(errorMsg)
+  })
 }
--- src/utils/web3-adapter.js
+++ src/utils/web3-adapter.js
@@ -1,7 +1,7 @@
 import Web3 from 'web3'
 import Web3Adapter from '@gnosis.pm/safe-web3-lib'
 
-const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545')
+const web3 = new Web3(Web3.givenProvider)
 const safeOwner = "0x2f05FadE3F3030b387eCA20f7f7d5f5b12B8Dc06"
 
 export const ethAdapter = new Web3Adapter({
