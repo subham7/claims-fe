ea7415531bf73a1bca596640d45946491e3c0f19
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2022-05-31 14:49:19 +0530

    updated abis and contract address

--- src/api/index.js
+++ src/api/index.js
@@ -10,7 +10,7 @@ import {onboard} from "../utils/wallet"
 // Global variables
 const MAIN_API_URL = 'http://ec2-65-0-105-40.ap-south-1.compute.amazonaws.com:4000/v1/'
 // export const FACTORY_CONTRACT_ADDRESS = '0x585d26CE6E1D28C334E22b307d43F32D5bF283Dd'
-export const FACTORY_CONTRACT_ADDRESS = '0x23fcD67A37848De5917705Aa55C82f17E142be3a'
+export const FACTORY_CONTRACT_ADDRESS = '0xE8269328F9B28AF72E37843fEc0940AAf3e8f281'
 export const USDC_CONTRACT_ADDRESS = '0x484727B6151a91c0298a9D2b9fD84cE3bc6BC4E3'
 const RINKEBY_URL = "https://young-black-silence.rinkeby.quiknode.pro/16777add1b7c70fec1d3080f9d7a64e1bc3095df/"
 
