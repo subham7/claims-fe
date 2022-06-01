2798e22869ba0c0162a88ad838f0e92b614ded35
Subham Sengupta <subham@Subhams-MacBook-Pro.local>
2022-06-01 15:53:27 +0530

    Build issues solved

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,6 @@ import React from "react"
 import { makeStyles } from "@mui/styles"
 import Image from "next/image"
 import Tags from "./tags"
-import CardTabs from "./cardTabs"
 
 const useStyles = makeStyles({
   card: { marginTop: "50px" },
