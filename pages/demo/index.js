1e825f2f719c01f544d59bbaecf50cc1a14cd9e4
ArpitaGanatra <ganatraarpita@gmail.com>
2023-03-02 20:47:03 +0530

    build error fixed

--- pages/demo/index.js
+++ pages/demo/index.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from "react";
-import Layout from "../../src/components/layouts/layout3";
+import Layout from "../../src/components/layouts/layout2";
 import {
   Grid,
   Button,
