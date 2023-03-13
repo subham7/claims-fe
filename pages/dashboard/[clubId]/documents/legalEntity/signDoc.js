275634b31c93060112e06b03e445ee42f2ec1308
ArpitaGanatra <ganatraarpita@gmail.com>
2023-03-13 22:29:12 +0530

    build error

--- pages/dashboard/[clubId]/documents/legalEntity/signDoc.js
+++ pages/dashboard/[clubId]/documents/legalEntity/signDoc.js
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
-import Layout1 from '../../../../../src/components/layouts/layout1'
+import React, { useEffect, useState } from "react";
+import Layout1 from "../../../../../src/components/layouts/layout1";
 import html2canvas from "html2canvas";
 import dynamic from "next/dynamic";
 import { makeStyles } from "@mui/styles";
 import Web3 from "web3";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import { useRouter } from "next/router";
-const DocumentPDF = dynamic(() => import("../../../../pdfGenerator"), {
+const DocumentPDF = dynamic(() => import("../pdfGenerator"), {
   ssr: false,
 });
 
 const useStyles = makeStyles({
-    btn: {
-        width: '150px',
-      fontFamily: 'sans-serif',
-      fontSize: '18px',
-      border: 'none',
-      padding: '12px 20px',
-      color: 'white',
-      background: '#3B7AFD',
-      borderRadius: '6px',
-      cursor: 'pointer',
-    },
-    title: {
-        fontSize: '28px'
-    },
-    container: {
-        height: "90vh",
-        width:'100vw', 
-        display:'flex', 
-        flexDirection: 'column', 
-        alignItems: 'center', 
-        justifyContent:'center', 
-        marginTop: '60px'
-    },
-    signDiv: {
-        display: 'flex', 
-        justifyContent: 'space-between', 
-        alignItems:'center', 
-        width:'65%'
-    }
-})
+  btn: {
+    width: "150px",
+    fontFamily: "sans-serif",
+    fontSize: "18px",
+    border: "none",
+    padding: "12px 20px",
+    color: "white",
+    background: "#3B7AFD",
+    borderRadius: "6px",
+    cursor: "pointer",
+  },
+  title: {
+    fontSize: "28px",
+  },
+  container: {
+    height: "90vh",
+    width: "100vw",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "60px",
+  },
+  signDiv: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "65%",
+  },
+});
 
 const htmltoImage = () => {
   // const domElement1 = document.getElementById("result1");
@@ -63,53 +63,56 @@ const htmltoImage = () => {
 };
 
 const SignDoc = () => {
-    const classes = useStyles()
-    const [signedAcc, setSignedAcc] = useState('')
-    const [signDoc, setSignDoc] = useState(false)
-    const [signedHash, setSignedHash] = useState('')
+  const classes = useStyles();
+  const [signedAcc, setSignedAcc] = useState("");
+  const [signDoc, setSignDoc] = useState(false);
+  const [signedHash, setSignedHash] = useState("");
 
-    const router = useRouter()
-    const {clubId} = router.query
+  const router = useRouter();
+  const { clubId } = router.query;
 
-    const signDocumentHandler = async() => {
+  const signDocumentHandler = async () => {
+    try {
+      const web3 = new Web3(window.ethereum);
 
-        try {
-            const web3 = new Web3(window.ethereum);
+      // current account
+      const accounts = await web3.eth.getAccounts();
+      const currentAccount = accounts[0];
 
-            // current account
-            const accounts = await web3.eth.getAccounts()
-            const currentAccount = accounts[0]
-    
-            console.log(currentAccount)
-            const originalMessage = "YOUR_MESSAGE";
-      
-            // Signed message
-            const signedMessage = await web3.eth.personal.sign(
-              originalMessage,
-              currentAccount,
-              "test password!" // configure your own password here.
-            );
+      console.log(currentAccount);
+      const originalMessage = "YOUR_MESSAGE";
 
-            setSignedAcc(currentAccount)
-            setSignDoc(true)
-            setSignedHash(signedMessage)
+      // Signed message
+      const signedMessage = await web3.eth.personal.sign(
+        originalMessage,
+        currentAccount,
+        "test password!", // configure your own password here.
+      );
 
-          } catch (error) {
-            console.log(error)
-          }
+      setSignedAcc(currentAccount);
+      setSignDoc(true);
+      setSignedHash(signedMessage);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const uploadToLightHouseHandler = () => {
-      alert('Lighthouse not integrated yet!')
-      router.push(`/dashboard/${clubId}`)
-    }
+  const uploadToLightHouseHandler = () => {
+    alert("Lighthouse not integrated yet!");
+    router.push(`/dashboard/${clubId}`);
+  };
 
-//   const [formData, setFormData] = useState({
-//     firstName: "",
-//     lastName: "",
-//     email: "",
-//     password: "",
-//   });
+  const [client, setClient] = useState(false);
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
+  //   const [formData, setFormData] = useState({
+  //     firstName: "",
+  //     lastName: "",
+  //     email: "",
+  //     password: "",
+  //   });
 
   // const checkForEmptyFields = () => {
   //   for (const key of Object.keys(formData)) {
@@ -118,40 +121,49 @@ const SignDoc = () => {
   //   return true;
   // };
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//   };
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //   };
 
-//   const updateFormData = (event) =>
-//     setFormData({
-//       ...formData,
-//       [event.target.name]: event.target.value,
-//     });
+  //   const updateFormData = (event) =>
+  //     setFormData({
+  //       ...formData,
+  //       [event.target.name]: event.target.value,
+  //     });
 
-//   const { firstName, lastName, email, password } = formData;
+  //   const { firstName, lastName, email, password } = formData;
 
   return (
     <div>
-        <Layout1>
-            <div className={classes.container}>
-                <div className={classes.signDiv}> 
-                    <h2 className={classes.title}>Review and Confirm</h2>
-                    {!signDoc && <button onClick={signDocumentHandler} className={classes.btn}>Sign PDF</button>}
-                    {signDoc && <button onClick={uploadToLightHouseHandler} className={classes.btn}>Finish</button>}
+      <Layout1>
+        <div className={classes.container}>
+          <div className={classes.signDiv}>
+            <h2 className={classes.title}>Review and Confirm</h2>
+            {!signDoc && (
+              <button onClick={signDocumentHandler} className={classes.btn}>
+                Sign PDF
+              </button>
+            )}
+            {signDoc && (
+              <button
+                onClick={uploadToLightHouseHandler}
+                className={classes.btn}
+              >
+                Finish
+              </button>
+            )}
+          </div>
 
-                </div>
+          <DocumentPDF signedAcc={signedAcc} signedHash={signedHash} />
 
-                <DocumentPDF signedAcc={signedAcc} signedHash={signedHash} />
-
-
-        {/* <PDFViewer
+          {/* <PDFViewer
         //   showToolbar={false}
           style={{ height: "100%", width: "90vw" }}
         >
           <MyDocument title="Personal Doc" data={formData} />
         </PDFViewer> */}
-      </div>
-      {/* <button>
+        </div>
+        {/* <button>
         <PDFDownloadLink
           document={<MyDocument title="personal doc" data={formData || {}} />}
           fileName="formDataabc.pdf"
@@ -169,7 +181,7 @@ const SignDoc = () => {
         </PDFDownloadLink>
         Download
       </button> */}
-      {/* <button
+        {/* <button
         onClick={() => {
           htmltoImage().then((imgSrcArr) => {
             import("./PdfGenerator")
@@ -191,7 +203,7 @@ const SignDoc = () => {
       >
         Download PDF
       </button> */}
-      {/* {checkForEmptyFields() && (
+        {/* {checkForEmptyFields() && (
         <PDFDownloadLink
           document={<PdfFile data={formData} />}
           fileName="formDataabc.pdf"
@@ -208,8 +220,8 @@ const SignDoc = () => {
           }
         </PDFDownloadLink>
       )} */}
-      {/* <LineGraph /> */}
-      </Layout1>    
+        {/* <LineGraph /> */}
+      </Layout1>
     </div>
   );
 };
