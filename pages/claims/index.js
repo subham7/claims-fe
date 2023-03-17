66b5d562d41f1b171b2a451ca46e297950af9ba7
Aman-Mandal <002amankumar@gmail.com>
2023-03-17 14:57:41 +0530

    Added the cards on the index page to navigate claims page and current stations

---
+++ pages/claims/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import CustomCard from "../../src/components/card";
+import Card from "../../src/components/cards/card";
+import claim from "../../public/assets/images/treasury_image.png";
+import station from "../../public/assets/images/gov_image.png";
+
+import { height } from "@mui/system";
+
+
+const Claims = () => {
+  return (
+    // <div className={""} style={{background:  'url(/assets/images/gradients.png)', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '60px'}}>
+      
+    //   <Card imgSrc={station} title={"Manage Stations"} subtitle={'Create and manage stations with few clicks'} />
+    //   <Card imgSrc={claim} title={"Claim Tokens"} subtitle={'Get your airdrop tokens from here!'} />
+
+    // </div>
+    <>
+    <h2>Claims Page</h2>
+    </>
+  );
+};
+
+export default Claims;
--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,8 @@ import {
   addClubID,
   addClubRoute,
 } from "../src/redux/reducers/create";
+import claim from "../public/assets/images/treasury_image.png";
+import station from "../public/assets/images/gov_image.png";
 
 import {
   getExpiryTime,
@@ -33,8 +35,13 @@ import {
 } from "../src/utils/auth";
 import { loginToken, refreshToken } from "../src/api/auth";
 import { useConnectWallet } from "@web3-onboard/react";
+import NewCard from "../src/components/cards/card";
 
 const useStyles = makeStyles({
+  container: {
+    background: "url(/assets/images/gradients.png)",
+    minHeight: "100vh",
+  },
   yourClubText: {
     fontSize: "30px",
     color: "#F5F5F5",
@@ -75,6 +82,13 @@ const useStyles = makeStyles({
   profilePic: {
     borderRadius: "50%",
   },
+  cardContainer: {
+    display: "flex",
+    gap: "60px",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "90vh",
+  },
 });
 
 export default function App() {
@@ -85,6 +99,9 @@ export default function App() {
   const [clubData, setClubData] = useState([]);
   const [clubOwnerAddress, setClubOwnerAddress] = useState(null);
   const [noWalletMessage, setNoWalletMessage] = useState(null);
+
+  const [manageStation, setManageStation] = useState(false);
+
   const [open, setOpen] = useState(false);
   const router = useRouter();
   const walletAddress = wallet?.accounts[0].address;
@@ -109,7 +126,6 @@ export default function App() {
                   result.data.userAddress.length - 4,
                 ),
             );
-            // setFetched(true);
           }
         })
         .catch((error) => {
@@ -187,179 +203,213 @@ export default function App() {
     let imgUrl = tokenURI?.split("/");
   };
 
+  const showStationsHandler = () => {
+    setManageStation(true);
+  };
+
+  const claimsHandler = () => {
+    router.push("/claims");
+  };
+
   return (
     <Layout2 faucet={false}>
-      {clubFlow ? (
-        <Grid
-          container
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          mt={20}
-          mb={10}
-        >
-          <Grid item md={5}>
-            <Card>
-              <Grid container mt={2}>
-                <Grid item>
-                  <Typography className={classes.yourClubText}>
-                    Your clubs
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs
-                  sx={{ display: "flex", justifyContent: "flex-end" }}
-                >
-                  <Button
-                    startIcon={<AddIcon fontSize="large" />}
-                    variant="primary"
-                    onClick={handleCreateButtonClick}
-                  >
-                    Create club
-                  </Button>
-                </Grid>
-              </Grid>
-              <Divider className={classes.divider} />
-              <Stack spacing={3}>
-                {walletAddress ? (
-                  clubData.map((club, key) => {
-                    return (
-                      <ListItemButton
-                        component="a"
-                        key={key}
-                        onClick={(e) => {
-                          handleItemClick(clubData[key]);
-                        }}
-                      >
-                        {getImageURL(club.imageUrl)}
-                        <Grid container>
-                          <Grid item md={2}>
-                            <img
-                              src={club.imageUrl}
-                              width="80vw"
-                              alt="club_image"
-                              className={classes.profilePic}
-                            />
-                          </Grid>
-                          <Grid item md={6}>
-                            <Stack spacing={0}>
-                              <Typography className={classes.yourClubText}>
-                                {club.name}
-                              </Typography>
-                              <Typography className={classes.clubAddress}>
-                                {clubOwnerAddress}
-                              </Typography>
-                            </Stack>
-                          </Grid>
-                          <Grid
-                            item
-                            md={4}
-                            xs
-                            sx={{ display: "flex", justifyContent: "flex-end" }}
-                          >
-                            <Stack
-                              spacing={0}
-                              alignItems="flex-end"
-                              justifyContent="flex-end"
-                            >
-                              <Typography
-                                className={classes.createClubButton}
-                              ></Typography>
-                              <Typography className={classes.clubAddress}>
-                                {club.isAdmin ? "Admin" : "Member"}
-                              </Typography>
-                            </Stack>
-                          </Grid>
-                        </Grid>
-                      </ListItemButton>
-                    );
-                  })
-                ) : (
-                  <Grid
-                    container
-                    item
-                    justifyContent="center"
-                    alignItems="center"
-                  >
-                    <Typography>{noWalletMessage}</Typography>
-                  </Grid>
-                )}
-              </Stack>
-            </Card>
-          </Grid>
-        </Grid>
-      ) : (
-        <Grid
-          container
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid item mt={15}>
-            <img
-              className={classes.bannerImage}
-              src="/assets/images/start_illustration.svg"
+      <div className={classes.container}>
+
+
+        {!manageStation && clubFlow && (
+          <div className={classes.cardContainer}>
+            <NewCard
+              onClick={showStationsHandler}
+              imgSrc={station}
+              title={"Manage Stations"}
+              subtitle={"Create and manage stations with few clicks"}
             />
-          </Grid>
-          <Grid item mt={4}>
-            <Typography variant="mainHeading">Do more together</Typography>
-          </Grid>
-          <Grid item mt={4}>
-            <Typography variant="regularText">
-              Create or join a club in less than 60 seconds using StationX
-            </Typography>
-          </Grid>
-          {/* <Grid item m={4}>
-            <Button
-              variant="primary"
-              color="primary"
-              sx={{ mr: 2 }}
-              onClick={() => handleConnection()}
-            >
-              Connect Wallet
-            </Button>
-          </Grid> */}
-        </Grid>
-      )}
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        scroll="body"
-        PaperProps={{ classes: { root: classes.modalStyle } }}
-        fullWidth
-        maxWidth="lg"
-      >
-        <DialogContent sx={{ overflow: "hidden", backgroundColor: "#19274B" }}>
+            <NewCard
+              onClick={claimsHandler}
+              imgSrc={claim}
+              title={"Claim Tokens"}
+              subtitle={"Get your airdrop tokens from here!"}
+            />
+          </div>
+        )}
+
+        {manageStation && clubFlow ? (
           <Grid
             container
+            direction="row"
             justifyContent="center"
             alignItems="center"
-            direction="column"
-            mt={3}
+            mt={20}
+            mb={10}
           >
-            <Grid item pl={15}>
-              <img src="/assets/images/connected_world_wuay.svg" width="80%" />
-            </Grid>
-            <Grid item m={3}>
-              <Typography className={classes.dialogBox}>
-                You are in the wrong network, please switch to the correct
-                network by clicking the button provided below
-              </Typography>
-            </Grid>
-            <Grid item m={3}>
-              <Button
-                variant="primary"
-                onClick={() => {
-                  handleSwitchNetwork();
-                }}
-              >
-                Switch Network
-              </Button>
+            <Grid item md={5}>
+              <Card>
+                <Grid container mt={2}>
+                  <Grid item>
+                    <Typography className={classes.yourClubText}>
+                      Your clubs
+                    </Typography>
+                  </Grid>
+                  <Grid
+                    item
+                    xs
+                    sx={{ display: "flex", justifyContent: "flex-end" }}
+                  >
+                    <Button
+                      startIcon={<AddIcon fontSize="large" />}
+                      variant="primary"
+                      onClick={handleCreateButtonClick}
+                    >
+                      Create club
+                    </Button>
+                  </Grid>
+                </Grid>
+                <Divider className={classes.divider} />
+                <Stack spacing={3}>
+                  {walletAddress ? (
+                    clubData.map((club, key) => {
+                      return (
+                        <ListItemButton
+                          component="a"
+                          key={key}
+                          onClick={(e) => {
+                            handleItemClick(clubData[key]);
+                          }}
+                        >
+                          {getImageURL(club.imageUrl)}
+                          <Grid container>
+                            <Grid item md={2}>
+                              <img
+                                src={club.imageUrl}
+                                width="80vw"
+                                alt="club_image"
+                                className={classes.profilePic}
+                              />
+                            </Grid>
+                            <Grid item md={6}>
+                              <Stack spacing={0}>
+                                <Typography className={classes.yourClubText}>
+                                  {club.name}
+                                </Typography>
+                                <Typography className={classes.clubAddress}>
+                                  {clubOwnerAddress}
+                                </Typography>
+                              </Stack>
+                            </Grid>
+                            <Grid
+                              item
+                              md={4}
+                              xs
+                              sx={{
+                                display: "flex",
+                                justifyContent: "flex-end",
+                              }}
+                            >
+                              <Stack
+                                spacing={0}
+                                alignItems="flex-end"
+                                justifyContent="flex-end"
+                              >
+                                <Typography
+                                  className={classes.createClubButton}
+                                ></Typography>
+                                <Typography className={classes.clubAddress}>
+                                  {club.isAdmin ? "Admin" : "Member"}
+                                </Typography>
+                              </Stack>
+                            </Grid>
+                          </Grid>
+                        </ListItemButton>
+                      );
+                    })
+                  ) : (
+                    <Grid
+                      container
+                      item
+                      justifyContent="center"
+                      alignItems="center"
+                    >
+                      <Typography>{noWalletMessage}</Typography>
+                    </Grid>
+                  )}
+                </Stack>
+              </Card>
             </Grid>
           </Grid>
-        </DialogContent>
-      </Dialog>
+        ) : (
+          <>
+            {!manageStation && (
+              <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Grid item mt={15}>
+                  <img
+                    className={classes.bannerImage}
+                    src="/assets/images/start_illustration.svg"
+                  />
+                </Grid>
+                <Grid item mt={4}>
+                  <Typography variant="mainHeading">
+                    Do more together
+                  </Typography>
+                </Grid>
+                <Grid item mt={4}>
+                  <Typography variant="regularText">
+                    Create or join a club in less than 60 seconds using StationX
+                  </Typography>
+                </Grid>
+              </Grid>
+            )}
+          </>
+        )}
+
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          scroll="body"
+          PaperProps={{ classes: { root: classes.modalStyle } }}
+          fullWidth
+          maxWidth="lg"
+        >
+          <DialogContent
+            sx={{ overflow: "hidden", backgroundColor: "#19274B" }}
+          >
+            <Grid
+              container
+              justifyContent="center"
+              alignItems="center"
+              direction="column"
+              mt={3}
+            >
+              <Grid item pl={15}>
+                <img
+                  src="/assets/images/connected_world_wuay.svg"
+                  width="80%"
+                />
+              </Grid>
+              <Grid item m={3}>
+                <Typography className={classes.dialogBox}>
+                  You are in the wrong network, please switch to the correct
+                  network by clicking the button provided below
+                </Typography>
+              </Grid>
+              <Grid item m={3}>
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    handleSwitchNetwork();
+                  }}
+                >
+                  Switch Network
+                </Button>
+              </Grid>
+            </Grid>
+          </DialogContent>
+        </Dialog>
+      </div>
     </Layout2>
   );
 }
