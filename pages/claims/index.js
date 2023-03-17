1ff9cf0364d3988126b64bf63ca66c3be3fa18fb
Aman-Mandal <002amankumar@gmail.com>
2023-03-17 17:18:39 +0530

    Created claims page and claimsCard

--- pages/claims/index.js
+++ pages/claims/index.js
@@ -1,23 +1,140 @@
-import React from "react";
-import CustomCard from "../../src/components/card";
-import Card from "../../src/components/cards/card";
-import claim from "../../public/assets/images/treasury_image.png";
-import station from "../../public/assets/images/gov_image.png";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import Layout1 from "../../src/components/layouts/layout1";
+import settingsImg from "../../public/assets/images/settings.png";
+import { makeStyles } from "@mui/styles";
+import { useRouter } from "next/router";
+// import { getDocumentsByClubId } from "../../../../src/api/document";
+import { Card, Grid, Link, Typography } from "@mui/material";
+import LegalEntityModal from "../../src/components/modals/LegalEntityModal";
+import claimsBanner from "../../public/assets/images/claimsBanner.png";
+import ClaimsCard from "./ClaimsCard";
 
-import { height } from "@mui/system";
+const useStyles = makeStyles({
+  container: {
+    marginLeft: "80px",
+    marginTop: "120px",
+    display: "flex",
+    gap: "30px",
+  },
+  leftDiv: {
+    flex: "0.65",
+    margin: 0,
+  },
+  header: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+  },
+  title: {
+    fontSize: "46px",
+    fontWeight: "500",
+    alignSelf: "flex-start",
+    marginTop: "0px",
+    color: "white",
+  },
+  claimDoc: {
+    width: "130px",
+    fontFamily: "sans-serif",
+    fontSize: "16px",
+    border: "none",
+    padding: "18px 24px",
+    color: "white",
+    background: "#3B7AFD",
+    borderRadius: "12px",
+    cursor: "pointer",
+  },
+  rightDiv: {
+    flex: "0.3",
+  },
+  imgContainer: {
+    position: "relative",
+    width: "100%",
+  },
+  rightDiv_title: {
+    fontSize: "24px",
+    fontWeight: "500",
+    lineHeight: "30px",
+    color: "black",
+    margin: 0,
+  },
+  docLink: {
+    position: "absolute",
+    bottom: "0px",
+    color: "black",
+    textDecoration: "underline",
+    cursor: "pointer",
+  },
+  noClaim: {
+    width: "600px",
+    margin: "0 auto",
+    textAlign: "center",
+    border: "1px solid #FFFFFF1A",
+    borderRadius: "10px",
+    padding: "10px 30px",
+    marginTop: "20px",
+  },
+  noClaim_heading: {
+    fontSize: "18px",
+    fontWeight: "400",
+    color: "white",
+  },
+  noClaim_para: {
+    fontSize: "14px",
+    fontWeight: "400",
+    color: "lightgray",
+  },
+  proposalInfoCard: {
+    background: settingsImg,
+    backgroundColor: "#81f5f4",
+  },
+  proposalImg: {
+    position: "relative",
+  },
+});
 
+const DUMMY_DATA = [0, 1, 2];
 
 const Claims = () => {
-  return (
-    // <div className={""} style={{background:  'url(/assets/images/gradients.png)', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '60px'}}>
-      
-    //   <Card imgSrc={station} title={"Manage Stations"} subtitle={'Create and manage stations with few clicks'} />
-    //   <Card imgSrc={claim} title={"Claim Tokens"} subtitle={'Get your airdrop tokens from here!'} />
+  const classes = useStyles();
+  const router = useRouter();
 
-    // </div>
-    <>
-    <h2>Claims Page</h2>
-    </>
+  const createClaimHandler = () => {
+    router.push("/claims/createClaim");
+  };
+
+  return (
+    <div className={classes.container}>
+      {/* Left Side */}
+      <div className={classes.leftDiv}>
+        <div className={classes.header}>
+          <p className={classes.title}>Claims</p>
+          <button onClick={createClaimHandler} className={classes.claimDoc}>
+            Create
+          </button>
+        </div>
+
+        {!DUMMY_DATA && (
+          <div className={classes.noClaim}>
+            <p className={classes.noClaim_heading}>No claims found</p>
+            <p className={classes.noClaim_para}>
+              Create a legal entity for this Station & invite members to sign
+              the doucment by sharing a private link.
+            </p>
+          </div>
+        )}
+        {/* No claims exist */}
+
+        {DUMMY_DATA.map((item) => (
+          <ClaimsCard />
+        ))}
+      </div>
+
+      {/* Right Side */}
+      <div className={classes.rightDiv}>
+        <Image src={claimsBanner} alt="claimBanner" />
+      </div>
+    </div>
   );
 };
 
