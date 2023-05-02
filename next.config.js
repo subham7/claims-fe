8e81a2fe590aa41d578b5f7a2c825433d0620e37
ArpitaGanatra <ganatraarpita@gmail.com>
2023-05-02 15:50:57 +0530

    image loader revert

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   swcMinify: false,
   images: {
     domains: ["clubprofilepics.s3.ap-south-1.amazonaws.com", "./public/*"],
-    loader: "custom",
-    loaderFile: "./public/assets/images/monogram.png",
   },
 };
 
