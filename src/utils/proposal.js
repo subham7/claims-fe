f79d56eb5846e24206613385a9a84f2e8bbe089c
Aman-Mandal <002amankumar@gmail.com>
2023-05-18 14:59:59 +0530

    Deleted Files

--- src/utils/proposal.js
+++ src/utils/proposal.js
@@ -10,24 +10,7 @@ export const fetchProposals = async (clubId, type) => {
   if (proposalData.status !== 200) {
     return null;
   }
-  //   proposalData.then(async (result) => {
-  //     if (result.status != 200) {
-  //       console.log("error");
-  //       return null;
-  //     } else {
-  //       console.log(result.data);
-
-  //   console.log("gnosisAddress", gnosisAddress);
-
-  //   Promise.all(
-  //     result.data.map(async (proposal) => {
-  //       const proposalTxHash = await getProposalTxHash(proposal.proposalId);
-  //       if (proposalTxHash.data[0]) {
-  //         proposal["safeTxHash"] = proposalTxHash?.data[0].txHash;
-  //       }
-  //     }),
-  //   );
+ 
   else return proposalData.data;
-  // }
-  //   });
+
 };
