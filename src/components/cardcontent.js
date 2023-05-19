f7e67073dadaee035bcf41ea081e8ed75b42a001
ArpitaGanatra <ganatraarpita@gmail.com>
2023-05-19 17:03:03 +0530

    collectibles card data removed

--- src/components/cardcontent.js
+++ src/components/cardcontent.js
@@ -21,30 +21,6 @@ const useStyles = makeStyles({
 export default function CollectionCard(props) {
   const classes = useStyles();
   const { metadata, tokenName, tokenSymbol, nftData } = props;
-  const [image, setImage] = useState("");
-
-  const tokenURI = nftData?.token_uri;
-
-  const fetchData = async () => {
-    if (tokenURI.length)
-      if (
-        tokenURI?.startsWith("ipfs") ||
-        tokenURI?.startsWith("data") ||
-        tokenURI?.startsWith("Qm")
-      ) {
-        const res = await fetch(`https://ipfs.io/ipfs/${tokenURI}`);
-        const data = await res.json();
-        setImage(data?.image);
-      } else if (tokenURI?.startsWith("https")) {
-        const res = await fetch(tokenURI);
-        const data = await res.json();
-        setImage(data?.image);
-      }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [tokenURI]);
 
   const jsonString = nftData.metadata;
   const jsonObject = JSON.parse(jsonString);
@@ -53,7 +29,6 @@ export default function CollectionCard(props) {
 
   return (
     <>
-      {/* {image.length ? ( */}
       <Card sx={{ maxWidth: "350px", maxHeight: "519px", padding: 0 }}>
         <CardMedia
           variant="collectionImage"
@@ -63,8 +38,6 @@ export default function CollectionCard(props) {
           image={imgUrl}
         />
 
-        {/* <MediaRenderer src={tokenURI?} alt="A mp4 video" /> */}
-        {/* <IpfsImage hash={tokenURI?}></IpfsImage> */}
         <CardContent>
           <Typography
             fontSize={28}
@@ -74,24 +47,9 @@ export default function CollectionCard(props) {
           >
             {tokenName}
           </Typography>
-          <Grid container spacing={4}>
-            <Grid item xs={12} sm container>
-              <Stack>
-                <Typography variant="cardFont2">Floor Price</Typography>
-                <Typography variant="cardFont1">2.5 {tokenSymbol}</Typography>
-                <Typography variant="cardFont2">(5993.2 USD)</Typography>
-              </Stack>
-            </Grid>
-            <Grid item xs={12} sm container>
-              <Stack>
-                <Typography variant="cardFont2">Net returns</Typography>
-                <Typography className={classes.netamount}>+ $1272</Typography>
-              </Stack>
-            </Grid>
-          </Grid>
+          <Grid container spacing={4}></Grid>
         </CardContent>
       </Card>
-      {/* ) : null} */}
     </>
   );
 }
