8166adc9cf0c95976f3acf7962584c6edac74174
ArpitaGanatra <ganatraarpita@gmail.com>
2023-05-19 12:09:58 +0530

    added queued txns

--- pages/dashboard/[clubId]/proposal/index.js
+++ pages/dashboard/[clubId]/proposal/index.js
@@ -20,6 +20,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { makeStyles } from "@mui/styles";
 import { setProposalList } from "../../../../src/redux/reducers/proposal";
 import WrongNetworkModal from "../../../../src/components/modals/WrongNetworkModal";
+import Web3 from "web3";
+import { RPC_URL } from "../../../../src/api";
+import { Web3Adapter } from "@safe-global/protocol-kit";
+import Safe from "@safe-global/protocol-kit";
+import SafeApiKit from "@safe-global/api-kit";
+import {
+  getProposalByDaoAddress,
+  getProposalTxHash,
+} from "../../../../src/api/proposal";
 
 const useStyles = makeStyles({
   noProposal_heading: {
@@ -60,6 +69,14 @@ const Proposal = () => {
     return state.gnosis.networkHex;
   });
 
+  const GNOSIS_TRANSACTION_URL = useSelector((state) => {
+    return state.gnosis.transactionUrl;
+  });
+
+  const gnosisAddress = useSelector((state) => {
+    return state.club.clubData.gnosisAddress;
+  });
+
   const CLUB_NETWORK_ID = useSelector((state) => {
     return state.gnosis.clubNetworkId;
   });
@@ -91,6 +108,8 @@ const Proposal = () => {
   const isGovernanceActive =
     tokenType === "erc20" ? isGovernanceERC20 : isGovernanceERC721;
 
+  const [executionTransaction, setExecutionTransaction] = useState(null);
+
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -121,8 +140,6 @@ const Proposal = () => {
   }, [NETWORK_HEX, daoAddress]);
   const fetchProposalList = async (type = "all") => {
     const data = await fetchProposals(daoAddress, type);
-
-    console.log(data);
     dispatch(setProposalList(data));
   };
 
@@ -133,6 +150,45 @@ const Proposal = () => {
     setSelectedListItem(value);
     fetchProposalList(value);
   };
+
+  const getSafeService = useCallback(async () => {
+    const web3 = new Web3(RPC_URL);
+    const ethAdapter = new Web3Adapter({
+      web3,
+      signerAddress: localStorage.getItem("wallet"),
+    });
+    const safeService = new SafeApiKit({
+      txServiceUrl: GNOSIS_TRANSACTION_URL,
+      ethAdapter,
+    });
+    return safeService;
+  }, [GNOSIS_TRANSACTION_URL]);
+
+  const getExecutionTransaction = async () => {
+    const safeService = await getSafeService();
+    const proposalData = getProposalByDaoAddress(daoAddress);
+    const pendingTxs = await safeService.getPendingTransactions(
+      Web3.utils.toChecksumAddress(gnosisAddress),
+    );
+    const count = pendingTxs.count;
+    proposalData.then(async (result) => {
+      Promise.all(
+        result.data.map(async (proposal) => {
+          const proposalTxHash = await getProposalTxHash(proposal.proposalId);
+          if (proposalTxHash.data[0]) {
+            proposal["safeTxHash"] = proposalTxHash?.data[0].txHash;
+            if (
+              proposalTxHash.data[0].txHash ===
+              pendingTxs?.results[count - 1]?.safeTxHash
+            ) {
+              setExecutionTransaction(proposal);
+            }
+          }
+        }),
+      );
+    });
+  };
+  getExecutionTransaction();
 
   useEffect(() => {
     if (daoAddress) {
@@ -236,24 +292,45 @@ const Proposal = () => {
             </Grid>
             <Grid container spacing={3}>
               {proposalList?.length > 0 ? (
-                proposalList.map((proposal, key) => {
-                  return (
-                    <Grid
-                      item
-                      key={proposal.id}
-                      onClick={(e) => {
-                        handleProposalClick(proposalList[key]);
-                      }}
-                      md={12}
-                    >
-                      <ProposalCard
-                        proposal={proposal}
-                        indexKey={key}
-                        // fetched={fetched}
-                      />
-                    </Grid>
-                  );
-                })
+                <>
+                  {executionTransaction && (
+                    <>
+                      <h2>Queued Transactions</h2>
+                      <Grid
+                        item
+                        onClick={(e) => {
+                          handleProposalClick(executionTransaction);
+                        }}
+                        md={12}
+                      >
+                        <ProposalCard
+                          proposal={executionTransaction}
+                          // fetched={fetched}
+                          executionTransaction={true}
+                        />
+                      </Grid>
+                    </>
+                  )}
+                  {executionTransaction && <h2>Proposals</h2>}
+                  {proposalList.map((proposal, key) => {
+                    return (
+                      <Grid
+                        item
+                        key={proposal.id}
+                        onClick={(e) => {
+                          handleProposalClick(proposalList[key]);
+                        }}
+                        md={12}
+                      >
+                        <ProposalCard
+                          proposal={proposal}
+                          indexKey={key}
+                          // fetched={fetched}
+                        />
+                      </Grid>
+                    );
+                  })}
+                </>
               ) : (
                 <div className={classes.noProposal}>
                   <p className={classes.noProposal_heading}>
