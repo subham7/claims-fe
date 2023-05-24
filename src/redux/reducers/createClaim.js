83054ac332797f386b439e0bdb504c0f4ae241a7
ArpitaGanatra <ganatraarpita@gmail.com>
2023-05-24 09:56:10 +0530

    removed unwanted slices from claim reducer

--- src/redux/reducers/createClaim.js
+++ src/redux/reducers/createClaim.js
@@ -3,32 +3,12 @@ import { createSlice } from "@reduxjs/toolkit";
 export const slice = createSlice({
   name: "createClaim",
   initialState: {
-    userData: null,
     claimContractData: null,
-    claimContractAddress: null,
-    merkleLeaves: [],
-    claimsOn: null,
     claimEnabled: true,
   },
   reducers: {
-    addUserData: (state, action) => {
-      state.userData = action.payload;
-    },
-
     addClaimContractData: (state, action) => {
       state.claimContractData = action.payload;
-    },
-
-    addClaimContractAddress: (state, action) => {
-      state.claimContractAddress = action.payload;
-    },
-
-    addMerkleLeaves: (state, action) => {
-      state.merkleLeaves = action.payload;
-    },
-
-    addClaimsOn: (state, action) => {
-      state.claimsOn = action.payload;
     },
 
     addClaimEnabled: (state, action) => {
@@ -38,11 +18,8 @@ export const slice = createSlice({
 });
 
 export const {
-  addUserData,
   addClaimContractData,
-  addClaimContractAddress,
-  addMerkleLeaves,
-  addClaimsOn,
+
   addClaimEnabled,
 } = slice.actions;
 
