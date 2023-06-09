bc36dbef36eaa88b0274b6486f20713eed8532df
ArpitaGanatra <ganatraarpita@gmail.com>
2023-06-09 18:14:38 +0530

    added sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -20,7 +20,7 @@ import { styled } from "@mui/material/styles";
 import Fade from "@mui/material/Fade";
 import Link from "next/link";
 import Image from "next/image";
-// import { HiDocumentDuplicate } from "react-icons/hi";
+import { HiDocumentDuplicate } from "react-icons/hi";
 
 const useStyles = makeStyles({
   listItemIcon: {
@@ -364,24 +364,24 @@ const Sidebar = (props) => {
               </ListItemButton>
             </BootstrapTooltip>
 
-            {/* <BootstrapTooltip title="Documents" placement="left">
-            <ListItemButton
-              component="a"
-              onClick={(e) => {
-                router.push(`/dashboard/${clubId}/documents`, undefined, {
-                  shallow: true,
-                });
-              }}>
-              <ListItemIcon
-                className={
-                  page == 6
-                    ? classes.listItemIconSelected
-                    : classes.listItemIcon
-                }>
-                <HiDocumentDuplicate size={30} />
-              </ListItemIcon>
-            </ListItemButton>
-          </BootstrapTooltip> */}
+            <BootstrapTooltip title="Documents" placement="left">
+              <ListItemButton
+                component="a"
+                onClick={(e) => {
+                  router.push(`/dashboard/${clubId}/documents`, undefined, {
+                    shallow: true,
+                  });
+                }}>
+                <ListItemIcon
+                  className={
+                    page == 6
+                      ? classes.listItemIconSelected
+                      : classes.listItemIcon
+                  }>
+                  <HiDocumentDuplicate size={30} />
+                </ListItemIcon>
+              </ListItemButton>
+            </BootstrapTooltip>
           </List>
         )}
       </Drawer>
