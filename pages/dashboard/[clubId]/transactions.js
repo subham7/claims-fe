e994790bf21515cb93eb6eb8167c62247992038f
Ankit Dwij <ankitdwiz010@gmail.com>
2023-07-25 14:48:19 +0530

    Transactions page init

---
+++ pages/dashboard/[clubId]/transactions.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout1 from "@components/layouts/layout1";
+
+const Transactions = () => {
+  return (
+    <>
+      <Layout1 page={6}></Layout1>
+    </>
+  );
+};
+
+export default Transactions;
