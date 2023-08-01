42dc49c3e55a31afe1d0deb37c7862f1047959b2
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-08-01 17:51:34 +0530

    styling changes

--- src/components/createClubComps/Step1.js
+++ src/components/createClubComps/Step1.js
@@ -3,7 +3,7 @@ import { TextField, Typography } from "@components/ui";
 
 export default function Step1(props) {
   return (
-    <div className="f-d f-vt f-v-s t-pad-d">
+    <div className="f-d f-vt f-v-s t-pad-d w-100">
       <Typography variant="body" className="text-blue">
         Basic info
       </Typography>
