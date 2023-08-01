79741630ac9bc8d2ce7c15427b382524a67f9036
Aman-Mandal <002amankumar@gmail.com>
2023-08-01 18:11:02 +0530

    Changed text for total nft supply in settings

--- src/components/settingsComps/SettingsInfo.js
+++ src/components/settingsComps/SettingsInfo.js
@@ -623,7 +623,9 @@ const SettingsInfo = ({
                   <Stack spacing={1}>
                     {daoDetails.isGovernance && (
                       <Typography variant="settingText">
-                        Total Raise Amount
+                        {tokenType === "erc721"
+                          ? " Total NFT Supply"
+                          : "Total Raise Amount"}
                       </Typography>
                     )}
 
