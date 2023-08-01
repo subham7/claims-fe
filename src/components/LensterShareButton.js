5bbdeef86c95751104b44b42ef0bef37d687c697
ArpitaGanatra <ganatraarpita@gmail.com>
2023-08-01 13:35:06 +0530

    fix: lens share text

--- src/components/LensterShareButton.js
+++ src/components/LensterShareButton.js
@@ -28,7 +28,7 @@ const LensterShareButton = ({ daoName, daoAddress, message = "" }) => {
             : `Just joined ${daoName} Station on stationxnetwork!`
         }&url=${
           window.location.origin
-        }/join/${daoAddress}&via=StationX&hashtags=lens,web3,stationX`}
+        }/join/${daoAddress}&hashtags=lens,web3,stationX`}
         style={{
           color: "#fff",
           fontWeight: "600",
