6bddfb56dae5cc4960e999ec13bf3f8076590aaa
bhavya2611 <bhavya.y.mehta@gmail.com>
2023-08-22 11:03:38 +0530

    updated next btn link routing

--- src/components/documents/membersSign.js
+++ src/components/documents/membersSign.js
@@ -86,8 +86,7 @@ const MembersSign = ({ daoAddress, membersSign }) => {
     onSubmit: (values) => {
       dispatch(addMembersData(values));
       dispatch(addEncryptedLink(membersSign));
-
-      router.push(`/documents/${daoAddress}/sign`);
+      router.push(`/documents/${daoAddress}/sign/${membersSign}`);
     },
   });
 
