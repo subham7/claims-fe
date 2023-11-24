e5bf9b4e75e986e06bd26a888d43a5bf0ac50a6a
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2023-11-24 17:28:23 +0530

    flow added for a first time whitelisted user

--- src/components/cards/InviteCard.js
+++ src/components/cards/InviteCard.js
@@ -5,6 +5,7 @@ import { whitelistUser } from "api/invite/invite";
 import React, { useState } from "react";
 import { useAccount } from "wagmi";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import { AiFillInfoCircle } from "react-icons/ai";
 
 const useStyles = makeStyles({
   card: {
@@ -41,7 +42,7 @@ const useStyles = makeStyles({
       "transparent linear-gradient(45deg, #2E55FF 0%, #FF279C 100%) 0% 0% no-repeat padding-box",
   },
   img: {
-    maxWidth: "70%",
+    maxWidth: "60%",
     marginBottom: "20px",
   },
   link: {
@@ -56,6 +57,25 @@ const useStyles = makeStyles({
     height: "16px",
     width: "16px",
   },
+  inviteCode: {
+    border: "1px solid white",
+    borderRadius: "10px",
+    padding: "12px",
+    borderStyle: "dashed",
+  },
+  warningContainer: {
+    color: "#ffb74d",
+    background: "#ffb74d30",
+    padding: "4px 8px",
+    borderRadius: "8px",
+    width: "fit-content",
+    display: "flex",
+    alignItems: "center",
+    gap: "8px",
+    marginBottom: "20px",
+    marginTop: "12px",
+    fontSize: "10px",
+  },
 });
 
 const InviteCard = ({ setIsUserWhitelisted }) => {
@@ -63,6 +83,8 @@ const InviteCard = ({ setIsUserWhitelisted }) => {
   const [value, setValue] = useState("");
   const { address } = useAccount();
   const [loading, setLoading] = useState(false);
+  const [showInviteCode, setShowInviteCode] = useState(false);
+  const [newInviteCode, setNewInviteCode] = useState("");
 
   const onClick = async () => {
     try {
@@ -72,7 +94,8 @@ const InviteCard = ({ setIsUserWhitelisted }) => {
         referralCode: value.toUpperCase(),
       });
       if (response) {
-        setIsUserWhitelisted(true);
+        setShowInviteCode(true);
+        setNewInviteCode(response.referralCode);
       }
     } catch (e) {
       console.error("Error whitelisting user", e);
@@ -90,43 +113,72 @@ const InviteCard = ({ setIsUserWhitelisted }) => {
           className={classes.img}
           alt="whitelist img"
         />
-        <div>
-          <Typography variant="heading">25,189 people ahead of you.</Typography>
-          <Typography variant="body">
-            {`To gain insights from users into how we can make StationX better,
+        {!showInviteCode && (
+          <div>
+            <Typography variant="heading">
+              25,189 people ahead of you.
+            </Typography>
+            <Typography variant="body">
+              {`To gain insights from users into how we can make StationX better,
             we’re currently invite-only.`}
-          </Typography>
-        </div>
+            </Typography>
+          </div>
+        )}
       </div>
       <div className={classes.contentDiv}>
         <div>
-          <Typography variant="heading">Already got your invite?</Typography>
+          <Typography variant="heading">
+            {showInviteCode
+              ? "You've been whitelisted!"
+              : "Already got your invite?"}
+          </Typography>
           <Typography variant="body">
-            {`If you don't have an invite code, ask an existing user to invite you or
+            {showInviteCode
+              ? "Share this code to whitelist a friend with a beta access to create stations on StationX"
+              : `If you don't have an invite code, ask an existing user to invite you or
         sign up on the waitlist to get access`}
           </Typography>
         </div>
         <div className={classes.inputDiv}>
-          <Typography>Enter 6-digit code</Typography>
-          <TextField
-            fullWidth
-            label="Invite code"
-            variant="outlined"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            inputProps={{ style: { textTransform: "uppercase" } }}
-          />
-          <Typography
-            onClick={() => window.open("https://tally.so/r/nG64GQ", "_blank")}
-            className={classes.link}>
-            {`I don't have an invite code`}{" "}
-            <OpenInNewIcon className={classes.icon} />
+          <Typography>
+            {showInviteCode ? "One-time invite code:" : "Enter 6-digit code"}
           </Typography>
+          {showInviteCode ? (
+            <div className={classes.inviteCode}>{newInviteCode}</div>
+          ) : (
+            <TextField
+              fullWidth
+              label="Invite code"
+              variant="outlined"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              inputProps={{ style: { textTransform: "uppercase" } }}
+            />
+          )}
+          {showInviteCode ? (
+            <div className={classes.warningContainer}>
+              <AiFillInfoCircle size={20} />
+              <Typography>
+                This code will never be shown to you again
+              </Typography>
+            </div>
+          ) : (
+            <Typography
+              onClick={() => window.open("https://tally.so/r/nG64GQ", "_blank")}
+              className={classes.link}>
+              {`I don't have an invite code`}{" "}
+              <OpenInNewIcon className={classes.icon} />
+            </Typography>
+          )}
         </div>
         <div>
-          <Button onClick={onClick}>
-            {loading ? <CircularProgress size={24} /> : "Submit"}{" "}
-          </Button>
+          {showInviteCode ? (
+            <Button onClick={() => setIsUserWhitelisted(true)}>Done</Button>
+          ) : (
+            <Button onClick={onClick}>
+              {loading ? <CircularProgress size={24} /> : "Submit"}{" "}
+            </Button>
+          )}
         </div>
       </div>
     </div>
