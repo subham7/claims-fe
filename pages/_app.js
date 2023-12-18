43fffdaa917d23b3b7db94527f906117939edd22
Aman-Mandal <002amankumar@gmail.com>
2023-12-18 20:00:34 +0530

    Added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -27,6 +27,7 @@ import {
   scrollMainnet,
   mantaMainnet,
 } from "utils/constants";
+import Script from "next/script";
 
 const API_URL = "https://api.lens.dev";
 
@@ -61,36 +62,50 @@ const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme()}>
-      <ApolloProvider client={apolloClient}>
-        <WagmiConfig config={wagmiConfig}>
-          <Provider store={store}>
-            {/* <AnnouncementProvider>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+        `}
+      </Script>
+      <ThemeProvider theme={theme()}>
+        <ApolloProvider client={apolloClient}>
+          <WagmiConfig config={wagmiConfig}>
+            <Provider store={store}>
+              {/* <AnnouncementProvider>
               <AnnouncementBar /> */}
-            <Component {...pageProps} />
-            {/* </AnnouncementProvider> */}
-          </Provider>
-        </WagmiConfig>
-        <Web3Modal
-          chainImages={{
-            59144: "/assets/icons/linea-mainnet.webp",
-            8453: "/assets/icons/base-mainnet.png",
-            5000: "/assets/icons/mantle-mainnet.png",
-            169: "/assets/icons/manta.png",
-            534352: "/assets/icons/scroll.jpeg",
-            167007: "/assets/icons/taiko.jpeg",
-          }}
-          themeMode="light"
-          themeVariables={{
-            "--w3m-overlay-background-color": "#00000088",
-            "--w3m-accent-color": "#000",
-            "--w3m-background-color": "#000",
-          }}
-          projectId={projectId}
-          ethereumClient={ethereumClient}
-        />
-      </ApolloProvider>
-    </ThemeProvider>
+              <Component {...pageProps} />
+              {/* </AnnouncementProvider> */}
+            </Provider>
+          </WagmiConfig>
+          <Web3Modal
+            chainImages={{
+              59144: "/assets/icons/linea-mainnet.webp",
+              8453: "/assets/icons/base-mainnet.png",
+              5000: "/assets/icons/mantle-mainnet.png",
+              169: "/assets/icons/manta.png",
+              534352: "/assets/icons/scroll.jpeg",
+              167007: "/assets/icons/taiko.jpeg",
+            }}
+            themeMode="light"
+            themeVariables={{
+              "--w3m-overlay-background-color": "#00000088",
+              "--w3m-accent-color": "#000",
+              "--w3m-background-color": "#000",
+            }}
+            projectId={projectId}
+            ethereumClient={ethereumClient}
+          />
+        </ApolloProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
