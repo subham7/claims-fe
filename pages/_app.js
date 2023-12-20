ab9da950c642a80cb91f9b00f6ad5e33500a1956
Bhavya Mehta <bhavya.y.mehta@gmail.com>
2023-12-20 11:00:38 +0530

    gtag issue

--- pages/_app.js
+++ pages/_app.js
@@ -88,7 +88,7 @@ function MyApp({ Component, pageProps }) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+          gtag('config', "${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}");
         `}
       </Script>
       <ThemeProvider theme={theme()}>
