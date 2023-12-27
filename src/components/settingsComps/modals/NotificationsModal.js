0c2889e8e45d4475a3ec32c8704b5d64817f588e
Ankit Dwij <ankitdwiz010@gmail.com>
2023-12-27 12:31:21 +0530

    added cross btn

--- src/components/settingsComps/modals/NotificationsModal.js
+++ src/components/settingsComps/modals/NotificationsModal.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Typography, Dialog, DialogContent } from "@mui/material";
+import { Typography, Dialog, DialogContent, IconButton } from "@mui/material";
 import { Button } from "@components/ui";
 import { makeStyles } from "@mui/styles";
 import Link from "next/link";
+import Close from "@mui/icons-material/Close";
 
 const useStyles = makeStyles({
   container: { width: "660px" },
@@ -10,10 +11,7 @@ const useStyles = makeStyles({
     backgroundColor: "#151515",
   },
   heading: {
-    fontSize: "28px !important",
-    marginBottom: "20px  !important",
-    fontFamily: "inherit  !important",
-    fontWeight: "500  !important",
+    fontSize: "24px !important",
   },
   textGray: {
     fontSize: "16px !important",
@@ -32,7 +30,7 @@ const useStyles = makeStyles({
     margin: "12px 0px",
     padding: "10px",
     borderRadius: "6px",
-    backgroundColor: "#000",
+    backgroundColor: "#282828",
   },
   redCode: {
     color: "#f22c3d",
@@ -53,9 +51,20 @@ const NotificationsModal = ({ open, onClose }) => {
       <DialogContent
         sx={{
           overflow: "hidden",
-          backgroundColor: "#202020",
+          backgroundColor: "#181818",
           padding: "1.5rem",
         }}>
+        <IconButton
+          aria-label="close"
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: "grey",
+          }}>
+          <Close />
+        </IconButton>
         <div>
           <Typography className={classes.heading}>Add notifications</Typography>
           <Typography className={classes.textGray}>
