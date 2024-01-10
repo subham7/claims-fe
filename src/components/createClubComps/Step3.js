cabad2cc5fc7415d928df333af21a07be16b4a8b
Aman-Mandal <002amankumar@gmail.com>
2024-01-10 17:59:56 +0530

    Fixed existing safe minor bug

--- src/components/createClubComps/Step3.js
+++ src/components/createClubComps/Step3.js
@@ -117,51 +117,52 @@ export default function Step3(props) {
       </ToggleButtonGroup>
 
       {/* {props.formik.values.deploySafe} */}
-      {props.formik.values.deploySafe === "oldSafe" && (
-        <>
-          <Typography variant="body" className="text-blue">
-            Select from existing multi-sig wallet(s)
-          </Typography>
-          <Autocomplete
-            name="safeAddress"
-            className={classes.textField}
-            options={allSafeAddresses}
-            onChange={(e, newValue) => {
-              props.formik.setFieldValue("safeAddress", newValue);
-            }}
-            renderInput={(params) => (
-              <TextField
-                name="safeAddress"
-                {...params}
-                label="Safe address"
-                variant="outlined"
-                placeholder="0x00"
-                error={
-                  props.formik.touched.safeAddress &&
-                  Boolean(props.formik.errors.safeAddress)
-                }
-                helperText={
-                  props.formik.touched.safeAddress &&
-                  props.formik.errors.safeAddress
-                }
-              />
-            )}
-          />
+      {props.formik.values.deploySafe === "oldSafe" &&
+        allSafeAddresses?.length > 0 && (
+          <>
+            <Typography variant="body" className="text-blue">
+              Select from existing multi-sig wallet(s)
+            </Typography>
+            <Autocomplete
+              name="safeAddress"
+              className={classes.textField}
+              options={allSafeAddresses}
+              onChange={(e, newValue) => {
+                props.formik.setFieldValue("safeAddress", newValue);
+              }}
+              renderInput={(params) => (
+                <TextField
+                  name="safeAddress"
+                  {...params}
+                  label="Safe address"
+                  variant="outlined"
+                  placeholder="0x00"
+                  error={
+                    props.formik.touched.safeAddress &&
+                    Boolean(props.formik.errors.safeAddress)
+                  }
+                  helperText={
+                    props.formik.touched.safeAddress &&
+                    props.formik.errors.safeAddress
+                  }
+                />
+              )}
+            />
 
-          <p
-            style={{
-              margin: "0",
-              color:
-                props.ownerHelperText ===
-                  "Owners of the safe does not match with the admins of the DAO" ||
-                props.ownerHelperText === "Invalid gnosis address"
-                  ? "red"
-                  : "#dcdcdc",
-            }}>
-            {props.ownerHelperText}
-          </p>
-        </>
-      )}
+            <p
+              style={{
+                margin: "0",
+                color:
+                  props.ownerHelperText ===
+                    "Owners of the safe does not match with the admins of the DAO" ||
+                  props.ownerHelperText === "Invalid gnosis address"
+                    ? "red"
+                    : "#dcdcdc",
+              }}>
+              {props.ownerHelperText}
+            </p>
+          </>
+        )}
 
       {props.formik.values.deploySafe === "oldSafe" &&
         props.formik.values.safeAddress?.length > 0 && (
