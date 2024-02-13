c58b6b226173a128ca816396f29264d131440ac5
Aman-Mandal <002amankumar@gmail.com>
2024-02-13 14:23:28 +0530

    Uncommented code

--- src/components/AnnouncementBar.js
+++ src/components/AnnouncementBar.js
@@ -1,83 +1,83 @@
-// import React, { useContext, useEffect } from "react";
-// import { AnnouncementContext } from "./AnnouncementContext";
-// import CloseIcon from "@mui/icons-material/Close";
-// import { useRouter } from "next/router";
-// import { useNetwork } from "wagmi";
+import React, { useContext, useEffect } from "react";
+import { AnnouncementContext } from "./AnnouncementContext";
+import CloseIcon from "@mui/icons-material/Close";
+import { useRouter } from "next/router";
+import { useNetwork } from "wagmi";
 
-// const AnnouncementBar = () => {
-//   const { showAnnouncement, closeAnnouncement, openAnnouncement } =
-//     useContext(AnnouncementContext);
-//   const router = useRouter();
+const AnnouncementBar = () => {
+  const { showAnnouncement, closeAnnouncement, openAnnouncement } =
+    useContext(AnnouncementContext);
+  const router = useRouter();
 
-//   const [daoAddress] = router?.query?.slug ?? [];
-//   const { chain } = useNetwork();
-//   const networkId = "0x" + chain?.id.toString(16);
+  const [daoAddress] = router?.query?.slug ?? [];
+  const { chain } = useNetwork();
+  const networkId = "0x" + chain?.id.toString(16);
 
-//   // useEffect(() => {
-//   //   if (
-//   //     router.pathname.includes("staking") ||
-//   //     router.pathname.includes("join")
-//   //   ) {
-//   //     closeAnnouncement();
-//   //   } else {
-//   //     openAnnouncement();
-//   //   }
-//   // }, [router.pathname]);
+  useEffect(() => {
+    if (
+      router.pathname.includes("staking") ||
+      router.pathname.includes("join")
+    ) {
+      closeAnnouncement();
+    } else {
+      openAnnouncement();
+    }
+  }, [router.pathname]);
 
-//   if (!showAnnouncement) {
-//     return null; // Hide the announcement bar if showAnnouncement is false
-//   }
+  if (!showAnnouncement) {
+    return null; // Hide the announcement bar if showAnnouncement is false
+  }
 
-//   if (networkId !== "0x1") {
-//     return null;
-//   }
+  if (networkId !== "0x1") {
+    return null;
+  }
 
-//   return (
-//     <div
-//       style={{
-//         display: "flex",
-//         background: "#fff",
-//         alignItems: "center",
-//         justifyContent: "center",
-//         position: "sticky",
-//         top: 0,
-//         width: "100vw",
-//         zIndex: "9999",
-//         padding: ".3rem 0",
-//       }}>
-//       <p style={{ margin: "0 0", color: "#000", fontWeight: 500 }}>
-//         {daoAddress
-//           ? "Earn Stars, Eigen points and other protocol points. Pool ETH inside the station & stake "
-//           : "Eigen Exploration is on. Earn stars, eigen points & other protocol rewards. Create your Defi Squad "}
+  return (
+    <div
+      style={{
+        display: "flex",
+        background: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        position: "sticky",
+        top: 0,
+        width: "100vw",
+        zIndex: "9999",
+        padding: ".3rem 0",
+      }}>
+      <p style={{ margin: "0 0", color: "#000", fontWeight: 500 }}>
+        {daoAddress
+          ? "Earn Stars, Eigen points and other protocol points. Pool ETH inside the station & stake "
+          : "Eigen Exploration is on. Earn stars, eigen points & other protocol rewards. Create your Defi Squad "}
 
-//         <span
-//           onClick={() => {
-//             if (daoAddress) {
-//               router.push(`/staking/${daoAddress}/${networkId}`);
-//             } else {
-//               router.push("/stations");
-//             }
-//           }}
-//           style={{
-//             textDecoration: "underline",
-//             cursor: "pointer",
-//             fontWeight: 700,
-//           }}>
-//           here
-//         </span>
-//       </p>
+        <span
+          onClick={() => {
+            if (daoAddress) {
+              router.push(`/staking/${daoAddress}/${networkId}`);
+            } else {
+              router.push("/stations");
+            }
+          }}
+          style={{
+            textDecoration: "underline",
+            cursor: "pointer",
+            fontWeight: 700,
+          }}>
+          here
+        </span>
+      </p>
 
-//       <CloseIcon
-//         onClick={closeAnnouncement}
-//         style={{
-//           position: "absolute",
-//           right: "2",
-//           cursor: "pointer",
-//           color: "#000",
-//         }}
-//       />
-//     </div>
-//   );
-// };
+      <CloseIcon
+        onClick={closeAnnouncement}
+        style={{
+          position: "absolute",
+          right: "2",
+          cursor: "pointer",
+          color: "#000",
+        }}
+      />
+    </div>
+  );
+};
 
-// export default AnnouncementBar;
+export default AnnouncementBar;
