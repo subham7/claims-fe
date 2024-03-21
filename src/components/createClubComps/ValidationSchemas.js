5e7fe29ccbd22b855552eb7ec7e13b0ac09d3524
Aman-Mandal <002amankumar@gmail.com>
2024-03-21 14:32:42 +0530

    Removed validation for nft price-per token

--- src/components/createClubComps/ValidationSchemas.js
+++ src/components/createClubComps/ValidationSchemas.js
@@ -332,7 +332,7 @@ export const getProposalValidationSchema = ({
         "price of token should be greater than current price",
         async (value, context) => {
           const { actionCommand } = context.parent;
-          if (actionCommand === 13) {
+          if (actionCommand === 13 && tokenType === "erc20") {
             try {
               const { pricePerToken, depositTokenAddress } = factoryData;
               const decimals = await getDecimals(depositTokenAddress);
