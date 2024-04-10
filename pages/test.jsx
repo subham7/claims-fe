e9efba8460e75ebf72b302d3f2fbf3040ac434ce
Aman-Mandal <002amankumar@gmail.com>
2024-04-10 19:43:17 +0530

    Finished UI of share page

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,12 +1,14 @@
 import Navbar from "@components/ui/Navbar/Navbar";
 import React from "react";
 import Spaces from "@components/(spaces)/Spaces";
+import Footer from "@components/(spaces)/Footer";
 
 const SpacesPage = () => {
   return (
     <div>
       <Navbar />
       <Spaces />
+      <Footer />
     </div>
   );
 };
