ebe86fc90194403fb8dd1f5b31c07200d39fa025
Aman-Mandal <002amankumar@gmail.com>
2024-06-20 17:26:48 +0530

    fix(signer-emmiter): Removed validation of member for adding admin

--- src/components/(settings)/TreasurySigner.jsx
+++ src/components/(settings)/TreasurySigner.jsx
@@ -12,7 +12,6 @@ import { handleSignMessage } from "utils/helper";
 import { createProposal } from "api/proposal";
 import { useSelector } from "react-redux";
 import { GoPencil } from "react-icons/go";
-import { isMember } from "utils/stationsSubgraphHelper";
 
 const TreasurySigner = ({
   clubData,
@@ -28,7 +27,6 @@ const TreasurySigner = ({
   const [showSaveButton, setShowSaveButton] = useState(false);
   const [showDeleteIcons, setShowDeleteIcons] = useState(false);
   const [clickedIndex, setClickedIndex] = useState(null);
-  const [showErrorText, setShowErrorText] = useState(false);
   const [type, setType] = useState(null);
 
   const { adminAddresses, currentSafeThreshold } = clubData;
@@ -56,22 +54,6 @@ const TreasurySigner = ({
   const submitHandler = async () => {
     try {
       setLoading(true);
-
-      if (type === "add") {
-        const isStationMember = await isMember(
-          newArr[newArr.length - 1],
-          daoAddress,
-          routeNetworkId,
-        );
-
-        if (!isStationMember?.users?.length > 0) {
-          setShowErrorText(true);
-          setLoading(false);
-          return;
-        } else {
-          setShowErrorText(false);
-        }
-      }
 
       const values = {
         ownerAddress:
@@ -191,17 +173,6 @@ const TreasurySigner = ({
           )}
         </div>
       ))}
-
-      {showErrorText && (
-        <Typography
-          variant="inherit"
-          fontSize={12}
-          color={"red"}
-          ml={1}
-          mt={0.5}>
-          Address is not a member of station
-        </Typography>
-      )}
 
       {isAdmin && showEditButton && adminAddresses?.length && (
         <button
